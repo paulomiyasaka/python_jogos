0569875774fbc6dc0747fb6586b742dbdf808015
paulomiyasaka <paulomiyasaka@gmail.com>
2026-01-13 01:24:30 -0300

    Aplicando CSRF
    Formulário de cadastrar novo jogo e de edição

--- jogoteca.py
+++ jogoteca.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 #from urllib.parse import quote_plus 
 #from sqlalchemy import create_engine, text
 
@@ -9,6 +10,7 @@ app.config.from_pyfile('config.py')
 #app.config['SQLALCHEMY_DATABASE_URI'] =  'mysql+mysqlconnector://root_python:python-cdip-bsb@mbs10065305/jogoteca'
 
 db = SQLAlchemy(app)
+csrf = CSRFProtect(app)
 
 from views import *
 
